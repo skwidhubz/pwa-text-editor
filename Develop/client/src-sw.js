f44b6ae9690c55c545c173d2840efe7b2e3a515f
skwidhubz <tommaxwellparker@gmail.com>
2023-04-10 08:31:14 +1000

    webpack config 1

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -28,3 +28,48 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute();
+
+
+
+    // // below is chatGPT respone for asset caching.
+
+    // importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+
+    // const CACHE_NAME = 'my-cache';
+    // const urlsToCache = [
+    //   '/',
+    //   '/styles.css',
+    //   '/script.js'
+    // ];
+
+    // // Cache assets when the service worker is installed
+    // self.addEventListener('install', event => {
+    //   event.waitUntil(
+    //     caches.open(CACHE_NAME)
+    //       .then(cache => cache.addAll(urlsToCache))
+    //   );
+    // });
+
+    // // Serve assets from the cache when possible
+    // self.addEventListener('fetch', event => {
+    //   event.respondWith(
+    //     caches.match(event.request)
+    //       .then(response => response || fetch(event.request))
+    //   );
+    // });
+
+    // // Delete old caches when the service worker is activated
+    // self.addEventListener('activate', event => {
+    //   event.waitUntil(
+    //     caches.keys()
+    //       .then(cacheNames => {
+    //         return Promise.all(
+    //           cacheNames.filter(cacheName => {
+    //             return cacheName.startsWith('my-') && cacheName !== CACHE_NAME;
+    //           }).map(cacheName => {
+    //             return caches.delete(cacheName);
+    //           })
+    //         );
+    //       })
+    //   );
+    // });
