029e6e8f5a11a6d370e8fa6d9d7a3ece3ff5a91d
skwidhubz <tommaxwellparker@gmail.com>
2023-04-14 19:47:07 +1000

    path change

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3005;
+const PORT = 3000;
 
-app.use(express.static('../client/dist'));
+app.use(express.static('../client'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
